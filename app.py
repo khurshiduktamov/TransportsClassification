d88ccafa2d5cbd6bdafdf4fe7fd110ae256cc6dc
Khurshid <40022458+khurshiduktamov@users.noreply.github.com>
2024-01-12 18:35:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pathlib
 import platform
 
 plt = platform.system()
-if plt =='Linux': pathlib.WindowsPath = pathlib.Posixpath
+if plt =='Linux': pathlib.WindowsPath = pathlib.PosixPath
 
 # Title
 st.title('Transport classification model')
